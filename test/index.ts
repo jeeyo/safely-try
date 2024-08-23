a51647c13fa754311e67fad0c5518ff93d851d06
Natchanon Nuntanirund <jeeyo@outlook.com>
2024-08-23 12:07:19 +0700

    add type inference test (#23)

--- test/index.ts
+++ test/index.ts
@@ -1,5 +1,32 @@
 import safelyTry from '../src';
 
+// https://stackoverflow.com/a/73461648/3562309
+export function assert<T extends never>() {}
+type TypeEqualityGuard<A,B> = Exclude<A,B> | Exclude<B,A>;
+
+describe('type assert', () => {
+  it('should infer correct data type (string)', () => {
+    const { data, error } = safelyTry(() => 'hello');
+    if (error === undefined) {
+      assert<TypeEqualityGuard<typeof data, string>>();
+    }
+  });
+
+  it('should infer correct data type (number)', () => {
+    const { data, error } = safelyTry(() => 1);
+    if (error === undefined) {
+      assert<TypeEqualityGuard<typeof data, number>>();
+    }
+  });
+
+  it('should infer correct error type', () => {
+    const { data, error } = safelyTry(() => 1);
+    if (data === undefined) {
+      assert<TypeEqualityGuard<typeof error, Error>>();
+    }
+  });
+});
+
 describe('safelyTry()', () => {
   it('should return { data: 1, error: undefined } when called with a synchronous function that returns 1 without errors', () => {
     expect(safelyTry(() => 1)).toStrictEqual({ data: 1, error: undefined });
